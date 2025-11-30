6e9e062ce9eae07de2437d428d019499e36b6186
Андрей Галкин <justscoundrel@yandex.ru>
2025-11-30 12:46:10 +0300

    пуш айфон

--- push-notification-nextjs-main/src/app/api/web-push/send/route.ts
+++
@@ -1,8 +0,0 @@
-import { sendNotification } from "@/notifications/NotificationSender";
-import { NextRequest } from "next/server";
-
-export async function POST(req: NextRequest) {
-    const { subscription, title, message } = await req.json();
-    await sendNotification(subscription, title, message);
-    return new Response(JSON.stringify({ message: "Push sent." }), {});
-}
